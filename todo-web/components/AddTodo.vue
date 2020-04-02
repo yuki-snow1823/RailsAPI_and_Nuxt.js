8ebdaa3c86a4d845d9ed2dffd5e5865e9d5ee00d
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-02 11:27:45 +0900

    first commit

---
+++ todo-web/components/AddTodo.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <!-- ここまでvuetifyの力 -->
+          <v-text-field v-model="title" :counter="10" label="todo" required></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-btn @click="handleSubmit">作成</v-btn>
+          <!-- v-on -->
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+      // v-modelと紐づいているからここも書き変わる
+    };
+  },
+ methods: {
+    handleSubmit() {
+      this.$emit("submit", this.title);  //この行を追加
+      // ボタン押した時にこれが発火し、importが書いてあるタイトルが飛んでいく
+      this.title = "";
+      // それで、入れた場所を白紙に戻している多分→あってる
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ todo-web/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
+    <!-- さらに何を入れるか定義 -->
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
+
+export default {
+  components: {
+    AddTodo,
+    TodoList
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        // こっちではこう動く。ように定義 todosにtitle入れるってことだと思う
+        title
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
